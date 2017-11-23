0c5a45866647cd83661e33430e331b546d07e816
Tobias Rickmann <tobirickmann@gmail.com>
2017-11-23 14:54:01 +0100

    modal open and close function

---
+++ public/js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    // Popup open/close
+    $('.open-popup').on('click', function() {
+        if($('body').hasClass('modal-enabled')) {
+            $('body').removeClass('modal-enabled');
+        } else {
+            $('body').addClass('modal-enabled');
+        }
+    });
+
+    $(document).on('click', '.close-modal', function() {
+        $('body').removeClass('modal-enabled');
+    });
+});
